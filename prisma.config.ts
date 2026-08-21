0a007610b1aa9d7031a07cfb8bee28128ae7a4bc
jelizavetakirsanova <jelizk@taltech.ee>
2026-08-21 14:51:01 +0300

    .

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from '@prisma/config';
+import 'dotenv/config';
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL,
   },
 });
